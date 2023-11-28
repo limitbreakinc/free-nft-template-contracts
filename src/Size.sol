7e4b7ec0ddb55a67c1d4e9518ca06f5a7b516e02
Nathan Gang <nathan.gang@limitbreak.com>
2023-11-27 21:00:01 -0700

    Add v2 templates

--- src/Size.sol
+++ src/Size.sol
@@ -34,6 +34,39 @@ import { NFT as V1_Constructable_FreeNFT_Merkle_ImmutableMinterRoyalties} from "
 import { NFT as V1_Constructable_FreeNFT_Merkle_MutableMinterRoyalties} from "./v1-templates/constructable/erc721c/merkle/mutable-minter-royalties/NFT.sol";
 import { NFT as V1_Constructable_FreeNFT_Merkle_SharedRoyalties} from "./v1-templates/constructable/erc721c/merkle/shared-royalties/NFT.sol";
 
+import { NFT as V2_Cloneable_AdventureFreeNFT_Airdrop_BasicRoyalties } from "./v2-templates/cloneable/adventure-erc721c/airdrop/basic-royalties/NFT.sol";
+import { NFT as V2_Cloneable_AdventureFreeNFT_Airdrop_ImmutableMinterRoyalties} from "./v2-templates/cloneable/adventure-erc721c/airdrop/immutable-minter-royalties/NFT.sol";
+import { NFT as V2_Cloneable_AdventureFreeNFT_Airdrop_MutableMinterRoyalties} from "./v2-templates/cloneable/adventure-erc721c/airdrop/mutable-minter-royalties/NFT.sol";
+import { NFT as V2_Cloneable_AdventureFreeNFT_Airdrop_SharedRoyalties} from "./v2-templates/cloneable/adventure-erc721c/airdrop/shared-royalties/NFT.sol";
+import { NFT as V2_Cloneable_AdventureFreeNFT_Merkle_BasicRoyalties} from "./v2-templates/cloneable/adventure-erc721c/merkle/basic-royalties/NFT.sol";
+import { NFT as V2_Cloneable_AdventureFreeNFT_Merkle_ImmutableMinterRoyalties} from "./v2-templates/cloneable/adventure-erc721c/merkle/immutable-minter-royalties/NFT.sol";
+import { NFT as V2_Cloneable_AdventureFreeNFT_Merkle_MutableMinterRoyalties} from "./v2-templates/cloneable/adventure-erc721c/merkle/mutable-minter-royalties/NFT.sol";
+import { NFT as V2_Cloneable_AdventureFreeNFT_Merkle_SharedRoyalties} from "./v2-templates/cloneable/adventure-erc721c/merkle/shared-royalties/NFT.sol";
+import { NFT as V2_Cloneable_FreeNFT_Airdrop_BasicRoyalties} from "./v2-templates/cloneable/erc721c/airdrop/basic-royalties/NFT.sol";
+import { NFT as V2_Cloneable_FreeNFT_Airdrop_ImmutableMinterRoyalties} from "./v2-templates/cloneable/erc721c/airdrop/immutable-minter-royalties/NFT.sol";
+import { NFT as V2_Cloneable_FreeNFT_Airdrop_MutableMinterRoyalties} from "./v2-templates/cloneable/erc721c/airdrop/mutable-minter-royalties/NFT.sol";
+import { NFT as V2_Cloneable_FreeNFT_Airdrop_SharedRoyalties} from "./v2-templates/cloneable/erc721c/airdrop/shared-royalties/NFT.sol";
+import { NFT as V2_Cloneable_FreeNFT_Merkle_BasicRoyalties} from "./v2-templates/cloneable/erc721c/merkle/basic-royalties/NFT.sol";
+import { NFT as V2_Cloneable_FreeNFT_Merkle_ImmutableMinterRoyalties} from "./v2-templates/cloneable/erc721c/merkle/immutable-minter-royalties/NFT.sol";
+import { NFT as V2_Cloneable_FreeNFT_Merkle_MutableMinterRoyalties} from "./v2-templates/cloneable/erc721c/merkle/mutable-minter-royalties/NFT.sol";
+import { NFT as V2_Cloneable_FreeNFT_Merkle_SharedRoyalties} from "./v2-templates/cloneable/erc721c/merkle/shared-royalties/NFT.sol";
+import { NFT as V2_Constructable_AdventureFreeNFT_Airdrop_BasicRoyalties} from "./v2-templates/constructable/adventure-erc721c/airdrop/basic-royalties/NFT.sol";
+import { NFT as V2_Constructable_AdventureFreeNFT_Airdrop_ImmutableMinterRoyalties} from "./v2-templates/constructable/adventure-erc721c/airdrop/immutable-minter-royalties/NFT.sol";
+import { NFT as V2_Constructable_AdventureFreeNFT_Airdrop_MutableMinterRoyalties} from "./v2-templates/constructable/adventure-erc721c/airdrop/mutable-minter-royalties/NFT.sol";
+import { NFT as V2_Constructable_AdventureFreeNFT_Airdrop_SharedRoyalties} from "./v2-templates/constructable/adventure-erc721c/airdrop/shared-royalties/NFT.sol";
+import { NFT as V2_Constructable_AdventureFreeNFT_Merkle_BasicRoyalties} from "./v2-templates/constructable/adventure-erc721c/merkle/basic-royalties/NFT.sol";
+import { NFT as V2_Constructable_AdventureFreeNFT_Merkle_ImmutableMinterRoyalties} from "./v2-templates/constructable/adventure-erc721c/merkle/immutable-minter-royalties/NFT.sol";
+import { NFT as V2_Constructable_AdventureFreeNFT_Merkle_MutableMinterRoyalties} from "./v2-templates/constructable/adventure-erc721c/merkle/mutable-minter-royalties/NFT.sol";
+import { NFT as V2_Constructable_AdventureFreeNFT_Merkle_SharedRoyalties} from "./v2-templates/constructable/adventure-erc721c/merkle/shared-royalties/NFT.sol";
+import { NFT as V2_Constructable_FreeNFT_Airdrop_BasicRoyalties} from "./v2-templates/constructable/erc721c/airdrop/basic-royalties/NFT.sol";
+import { NFT as V2_Constructable_FreeNFT_Airdrop_ImmutableMinterRoyalties} from "./v2-templates/constructable/erc721c/airdrop/immutable-minter-royalties/NFT.sol";
+import { NFT as V2_Constructable_FreeNFT_Airdrop_MutableMinterRoyalties} from "./v2-templates/constructable/erc721c/airdrop/mutable-minter-royalties/NFT.sol";
+import { NFT as V2_Constructable_FreeNFT_Airdrop_SharedRoyalties} from "./v2-templates/constructable/erc721c/airdrop/shared-royalties/NFT.sol";
+import { NFT as V2_Constructable_FreeNFT_Merkle_BasicRoyalties} from "./v2-templates/constructable/erc721c/merkle/basic-royalties/NFT.sol";
+import { NFT as V2_Constructable_FreeNFT_Merkle_ImmutableMinterRoyalties} from "./v2-templates/constructable/erc721c/merkle/immutable-minter-royalties/NFT.sol";
+import { NFT as V2_Constructable_FreeNFT_Merkle_MutableMinterRoyalties} from "./v2-templates/constructable/erc721c/merkle/mutable-minter-royalties/NFT.sol";
+import { NFT as V2_Constructable_FreeNFT_Merkle_SharedRoyalties} from "./v2-templates/constructable/erc721c/merkle/shared-royalties/NFT.sol";
+
 contract Size_V1_Cloneable_AdventureFreeNFT_Airdrop_BasicRoyalties is V1_Cloneable_AdventureFreeNFT_Airdrop_BasicRoyalties {}
 contract Size_V1_Cloneable_AdventureFreeNFT_Airdrop_ImmutableMinterRoyalties is V1_Cloneable_AdventureFreeNFT_Airdrop_ImmutableMinterRoyalties {}
 contract Size_V1_Cloneable_AdventureFreeNFT_Airdrop_MutableMinterRoyalties is V1_Cloneable_AdventureFreeNFT_Airdrop_MutableMinterRoyalties {}
@@ -352,6 +385,334 @@ contract Size_V1_Constructable_FreeNFT_Merkle_SharedRoyalties is V1_Constructabl
         MerkleMintConfiguration memory merkleMintConfiguration_,
         SharedRoyaltyConfiguration memory sharedRoyaltyConfiguration_)
     V1_Constructable_FreeNFT_Merkle_SharedRoyalties(
+        name_,
+        symbol_,
+        maxSupply_,
+        maxOwnerMints_,
+        merkleMintConfiguration_,
+        sharedRoyaltyConfiguration_) {}
+}
+
+// V2
+
+contract Size_V2_Cloneable_AdventureFreeNFT_Airdrop_BasicRoyalties is V2_Cloneable_AdventureFreeNFT_Airdrop_BasicRoyalties {}
+contract Size_V2_Cloneable_AdventureFreeNFT_Airdrop_ImmutableMinterRoyalties is V2_Cloneable_AdventureFreeNFT_Airdrop_ImmutableMinterRoyalties {}
+contract Size_V2_Cloneable_AdventureFreeNFT_Airdrop_MutableMinterRoyalties is V2_Cloneable_AdventureFreeNFT_Airdrop_MutableMinterRoyalties {}
+contract Size_V2_Cloneable_AdventureFreeNFT_Airdrop_SharedRoyalties is V2_Cloneable_AdventureFreeNFT_Airdrop_SharedRoyalties {}
+contract Size_V2_Cloneable_AdventureFreeNFT_Merkle_BasicRoyalties is V2_Cloneable_AdventureFreeNFT_Merkle_BasicRoyalties {}
+contract Size_V2_Cloneable_AdventureFreeNFT_Merkle_ImmutableMinterRoyalties is V2_Cloneable_AdventureFreeNFT_Merkle_ImmutableMinterRoyalties {}
+contract Size_V2_Cloneable_AdventureFreeNFT_Merkle_MutableMinterRoyalties is V2_Cloneable_AdventureFreeNFT_Merkle_MutableMinterRoyalties {}
+contract Size_V2_Cloneable_AdventureFreeNFT_Merkle_SharedRoyalties is V2_Cloneable_AdventureFreeNFT_Merkle_SharedRoyalties {}
+
+contract Size_V2_Cloneable_FreeNFT_Airdrop_BasicRoyalties is V2_Cloneable_FreeNFT_Airdrop_BasicRoyalties {}
+contract Size_V2_Cloneable_FreeNFT_Airdrop_ImmutableMinterRoyalties is V2_Cloneable_FreeNFT_Airdrop_ImmutableMinterRoyalties {}
+contract Size_V2_Cloneable_FreeNFT_Airdrop_MutableMinterRoyalties is V2_Cloneable_FreeNFT_Airdrop_MutableMinterRoyalties {}
+contract Size_V2_Cloneable_FreeNFT_Airdrop_SharedRoyalties is V2_Cloneable_FreeNFT_Airdrop_SharedRoyalties {}
+contract Size_V2_Cloneable_FreeNFT_Merkle_BasicRoyalties is V2_Cloneable_FreeNFT_Merkle_BasicRoyalties {}
+contract Size_V2_Cloneable_FreeNFT_Merkle_ImmutableMinterRoyalties is V2_Cloneable_FreeNFT_Merkle_ImmutableMinterRoyalties {}
+contract Size_V2_Cloneable_FreeNFT_Merkle_MutableMinterRoyalties is V2_Cloneable_FreeNFT_Merkle_MutableMinterRoyalties {}
+contract Size_V2_Cloneable_FreeNFT_Merkle_SharedRoyalties is V2_Cloneable_FreeNFT_Merkle_SharedRoyalties {}
+
+contract Size_V2_Constructable_AdventureFreeNFT_Airdrop_BasicRoyalties is V2_Constructable_AdventureFreeNFT_Airdrop_BasicRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSimultaneousQuests_, 
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxAirdropMints_,
+        address royaltyReceiver_, 
+        uint96 royaltyFeeNumerator_) 
+    V2_Constructable_AdventureFreeNFT_Airdrop_BasicRoyalties(
+        name_,
+        symbol_,
+        maxSimultaneousQuests_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxAirdropMints_,
+        royaltyReceiver_,
+        royaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_AdventureFreeNFT_Airdrop_ImmutableMinterRoyalties is V2_Constructable_AdventureFreeNFT_Airdrop_ImmutableMinterRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSimultaneousQuests_, 
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxAirdropMints_,
+        uint256 royaltyFeeNumerator_) 
+    V2_Constructable_AdventureFreeNFT_Airdrop_ImmutableMinterRoyalties(
+        name_,
+        symbol_,
+        maxSimultaneousQuests_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxAirdropMints_,
+        royaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_AdventureFreeNFT_Airdrop_MutableMinterRoyalties is V2_Constructable_AdventureFreeNFT_Airdrop_MutableMinterRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSimultaneousQuests_, 
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxAirdropMints_,
+        uint96 defaultRoyaltyFeeNumerator_) 
+    V2_Constructable_AdventureFreeNFT_Airdrop_MutableMinterRoyalties(
+        name_,
+        symbol_,
+        maxSimultaneousQuests_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxAirdropMints_,
+        defaultRoyaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_AdventureFreeNFT_Airdrop_SharedRoyalties is V2_Constructable_AdventureFreeNFT_Airdrop_SharedRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSimultaneousQuests_, 
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxAirdropMints_,
+        SharedRoyaltyConfiguration memory sharedRoyaltyConfiguration_) 
+    V2_Constructable_AdventureFreeNFT_Airdrop_SharedRoyalties(
+        name_,
+        symbol_,
+        maxSimultaneousQuests_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxAirdropMints_,
+        sharedRoyaltyConfiguration_) {}
+}
+
+contract Size_V2_Constructable_AdventureFreeNFT_Merkle_BasicRoyalties is V2_Constructable_AdventureFreeNFT_Merkle_BasicRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSimultaneousQuests_, 
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxMerkleMints_, 
+        uint256 permittedNumberOfMerkleRootChanges_,
+        address royaltyReceiver_, 
+        uint96 royaltyFeeNumerator_) 
+    V2_Constructable_AdventureFreeNFT_Merkle_BasicRoyalties(
+        name_,
+        symbol_,
+        maxSimultaneousQuests_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxMerkleMints_,
+        permittedNumberOfMerkleRootChanges_,
+        royaltyReceiver_,
+        royaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_AdventureFreeNFT_Merkle_ImmutableMinterRoyalties is V2_Constructable_AdventureFreeNFT_Merkle_ImmutableMinterRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSimultaneousQuests_, 
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxMerkleMints_, 
+        uint256 permittedNumberOfMerkleRootChanges_,
+        uint256 royaltyFeeNumerator_)
+    V2_Constructable_AdventureFreeNFT_Merkle_ImmutableMinterRoyalties(
+        name_,
+        symbol_,
+        maxSimultaneousQuests_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxMerkleMints_,
+        permittedNumberOfMerkleRootChanges_,
+        royaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_AdventureFreeNFT_Merkle_MutableMinterRoyalties is V2_Constructable_AdventureFreeNFT_Merkle_MutableMinterRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSimultaneousQuests_, 
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxMerkleMints_, 
+        uint256 permittedNumberOfMerkleRootChanges_,
+        uint96 defaultRoyaltyFeeNumerator_)
+    V2_Constructable_AdventureFreeNFT_Merkle_MutableMinterRoyalties(
+        name_,
+        symbol_,
+        maxSimultaneousQuests_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxMerkleMints_,
+        permittedNumberOfMerkleRootChanges_,
+        defaultRoyaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_AdventureFreeNFT_Merkle_SharedRoyalties is V2_Constructable_AdventureFreeNFT_Merkle_SharedRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSimultaneousQuests_, 
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        MerkleMintConfiguration memory merkleMintConfiguration_,
+        SharedRoyaltyConfiguration memory sharedRoyaltyConfiguration_)
+    V2_Constructable_AdventureFreeNFT_Merkle_SharedRoyalties(
+        name_,
+        symbol_,
+        maxSimultaneousQuests_,
+        maxSupply_,
+        maxOwnerMints_,
+        merkleMintConfiguration_,
+        sharedRoyaltyConfiguration_) {}
+}
+
+contract Size_V2_Constructable_FreeNFT_Airdrop_BasicRoyalties is V2_Constructable_FreeNFT_Airdrop_BasicRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxAirdropMints_,
+        address royaltyReceiver_, 
+        uint96 royaltyFeeNumerator_) 
+    V2_Constructable_FreeNFT_Airdrop_BasicRoyalties(
+        name_,
+        symbol_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxAirdropMints_,
+        royaltyReceiver_,
+        royaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_FreeNFT_Airdrop_ImmutableMinterRoyalties is V2_Constructable_FreeNFT_Airdrop_ImmutableMinterRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxAirdropMints_,
+        uint256 royaltyFeeNumerator_) 
+    V2_Constructable_FreeNFT_Airdrop_ImmutableMinterRoyalties(
+        name_,
+        symbol_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxAirdropMints_,
+        royaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_FreeNFT_Airdrop_MutableMinterRoyalties is V2_Constructable_FreeNFT_Airdrop_MutableMinterRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxAirdropMints_,
+        uint96 defaultRoyaltyFeeNumerator_) 
+    V2_Constructable_FreeNFT_Airdrop_MutableMinterRoyalties(
+        name_,
+        symbol_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxAirdropMints_,
+        defaultRoyaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_FreeNFT_Airdrop_SharedRoyalties is V2_Constructable_FreeNFT_Airdrop_SharedRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxAirdropMints_,
+        SharedRoyaltyConfiguration memory sharedRoyaltyConfiguration_) 
+    V2_Constructable_FreeNFT_Airdrop_SharedRoyalties(
+        name_,
+        symbol_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxAirdropMints_,
+        sharedRoyaltyConfiguration_) {}
+}
+
+contract Size_V2_Constructable_FreeNFT_Merkle_BasicRoyalties is V2_Constructable_FreeNFT_Merkle_BasicRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxMerkleMints_, 
+        uint256 permittedNumberOfMerkleRootChanges_,
+        address royaltyReceiver_, 
+        uint96 royaltyFeeNumerator_) 
+    V2_Constructable_FreeNFT_Merkle_BasicRoyalties(
+        name_,
+        symbol_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxMerkleMints_,
+        permittedNumberOfMerkleRootChanges_,
+        royaltyReceiver_,
+        royaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_FreeNFT_Merkle_ImmutableMinterRoyalties is V2_Constructable_FreeNFT_Merkle_ImmutableMinterRoyalties {
+        constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxMerkleMints_, 
+        uint256 permittedNumberOfMerkleRootChanges_,
+        uint256 royaltyFeeNumerator_)
+    V2_Constructable_FreeNFT_Merkle_ImmutableMinterRoyalties(
+        name_,
+        symbol_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxMerkleMints_,
+        permittedNumberOfMerkleRootChanges_,
+        royaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_FreeNFT_Merkle_MutableMinterRoyalties is V2_Constructable_FreeNFT_Merkle_MutableMinterRoyalties {
+    constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        uint256 maxMerkleMints_, 
+        uint256 permittedNumberOfMerkleRootChanges_,
+        uint96 defaultRoyaltyFeeNumerator_)
+    V2_Constructable_FreeNFT_Merkle_MutableMinterRoyalties(
+        name_,
+        symbol_,
+        maxSupply_,
+        maxOwnerMints_,
+        maxMerkleMints_,
+        permittedNumberOfMerkleRootChanges_,
+        defaultRoyaltyFeeNumerator_) {}
+}
+
+contract Size_V2_Constructable_FreeNFT_Merkle_SharedRoyalties is V2_Constructable_FreeNFT_Merkle_SharedRoyalties {
+        constructor(
+        string memory name_, 
+        string memory symbol_,
+        uint256 maxSupply_, 
+        uint256 maxOwnerMints_,
+        MerkleMintConfiguration memory merkleMintConfiguration_,
+        SharedRoyaltyConfiguration memory sharedRoyaltyConfiguration_)
+    V2_Constructable_FreeNFT_Merkle_SharedRoyalties(
         name_,
         symbol_,
         maxSupply_,
